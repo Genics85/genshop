6cff79bfdb162461ba5129b3a5a1c899dde724ce
unknown <eugeneamo85@gmail.com>
2023-01-04 22:58:42 +0000

    created auth context api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from "./context/authContext";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
