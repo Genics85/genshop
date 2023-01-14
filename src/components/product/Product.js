0f0b18d0fcc998be93ee2a46d371f2e73b2039f4
unknown <eugeneamo85@gmail.com>
2023-01-14 23:28:06 +0000

    addition and removal from cart added

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,10 +1,38 @@
+import { Col, Container, Image, Row } from "react-bootstrap";
+import { Dash, Plus } from "react-bootstrap-icons";
+import {useState} from "react";
 import "./product.scss";
 function Product() {
+    const[number,setNumber]=useState();
+
+    const handleAdd=()=>{
+        setNumber(number+1);
+    }
+    const handleRemove=()=>{
+        setNumber(number-1);
+    }
   return (
     <main>
-        
+      <Container>
+        <Row>
+          <Col className="col-12 col-md-7">
+            <Image className="mb-3" src="" />
+            <div className="buttons">
+              <Button onClick={handleAdd}>
+                <Plus />
+              </Button>
+              <>|</>
+              <Button onClick={handleRemove}>
+                <Dash />
+              </Button>
+            </div>
+            <p>{number}</p>
+          </Col>
+          <Col className="col-12 col-md-5"></Col>
+        </Row>
+      </Container>
     </main>
-  )
+  );
 }
 
-export default Product
+export default Product;
