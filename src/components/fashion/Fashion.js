a6064f4d043fbc1be32d690c6431824545642751
unknown <eugeneamo85@gmail.com>
2023-01-17 22:19:54 +0000

    creating product page for various category

--- src/components/fashion/Fashion.js
+++ src/components/fashion/Fashion.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import { Card, Container } from "react-bootstrap";
 function Fashion() {
   return (
-    <div>Fashion</div>
-  )
+    <div>
+      <Card>
+        <Container>
+          <Row>
+            <Col>
+              <Image src="" />
+              <p>
+                Amet sunt adipisicing minim deserunt. Eiusmod occaecat commodo
+                duis duis elit consectetur Lorem laborum consectetur velit.
+                Commodo eu Lorem quis magna fugiat fugiat. Ad consequat elit
+              </p>
+              <div>
+                <Button>Buy</Button>
+                <Button>Add to Cart</Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Card>
+    </div>
+  );
 }
 
-export default Fashion
+export default Fashion;
