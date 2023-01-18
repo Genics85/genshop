7923e05cad94d0070d53a1732187cbfefa75a550
unknown <eugeneamo85@gmail.com>
2023-01-18 23:37:39 +0000

    useRefresh token hook created

--- src/components/fashion/Fashion.js
+++ src/components/fashion/Fashion.js
@@ -1,9 +1,14 @@
 import React from "react";
 import SingleProduct from "../singleProduct/SingleProduct";
+import useRefreshToken from "../../hooks/useRefreshToken";
+
 function Fashion() {
+
+  const refresh=useRefreshToken();
   return (
     <div>
       <SingleProduct/>
+      <button onClick={()=>refresh()}> click me sucker</button>
     </div>
   );
 }
--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,12 +1,24 @@
-import{ React} from 'react'
-import useAuth from './useAuth';
-import axios from '../api/axios';
+import useAuth from "./useAuth";
+import axios from "../api/axios";
+import { useEffect } from "react";
 
 function useRefreshToken() {
-    
-  return (
-    <div>useRefreshToken</div>
-  )
+  const { setAuth } = useAuth();
+
+  
+  const refresh = async () => {
+    const response = await axios.get("user/refresh", {
+      withCredentials: true,
+    });
+    const accessToken = response?.data?.accessToken;
+    setAuth((prev) => {
+      console.log(accessToken);
+      console.log(JSON.stringify(prev));
+      return { ...prev, accessToken: accessToken };
+    });
+    return accessToken;
+  };
+  return refresh;
 }
 
-export default useRefreshToken
+export default useRefreshToken;
