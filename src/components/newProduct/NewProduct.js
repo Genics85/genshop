607b3e3a648689e2f8838f4769cdf017c75c73d5
unknown <eugeneamo85@gmail.com>
2023-01-21 01:44:45 +0000

    adding newproduct page done and finalized

--- src/components/newProduct/NewProduct.js
+++ src/components/newProduct/NewProduct.js
@@ -1,48 +1,63 @@
 import React, { useState } from "react";
 import "./newproduct.scss";
-import { Container, Col, Row, Form, Card, Button, Alert } from "react-bootstrap";
+import {
+  Container,
+  Col,
+  Row,
+  Form,
+  Card,
+  Button,
+  Alert,
+} from "react-bootstrap";
 import axios from "../../api/axios";
 
 function NewProduct() {
-  const [name,setName]=useState("");
-  const [price,setPrice]=useState("");
-  const [category,setCategory]=useState("");
-  const [image,setImage]= useState(null);
-  
-  const formData= new FormData();
-  formData.append("image",image);
-  formData.append("name",name);
-  formData.append("price",price);
-  formData.append("category",category)
-  
-  const handleName=(e)=>{
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [image, setImage] = useState(null);
+  const [success, setSuccess] = useState(false);
+
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("name", name);
+  formData.append("price", price);
+  formData.append("category", category);
+
+  const handleName = (e) => {
     setName(e.target.value);
-  }
-  const handlePrice=(e)=>{
+  };
+  const handlePrice = (e) => {
     setPrice(e.target.value);
-  }
-  const handleImage=(e)=>{
+  };
+  const handleImage = (e) => {
     setImage(e.target.files[0]);
-  }
-  const handleSelect=(e)=>{
+  };
+  const handleSelect = (e) => {
     setCategory(e.target.value);
+  };
+  const clearInputs=(e)=>{
+    setName("");
+    setPrice("");
+    setImage(null);
   }
-  
-  const handleSubmit=async(e)=>{
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const config={
-      headrs:{
-        'content-type':'multipart/form-data'
-      }
+    const config = {
+      headrs: {
+        "content-type": "multipart/form-data",
+      },
     };
     try {
-      const response = await axios.post("/product/upload",formData,config);
+      const response = await axios.post("/product/upload", formData, config);
+      if (response) setSuccess(true);
+      clearInputs();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
+  };
 
-  }
-  
   return (
     <div className="vh-100 center newproduct">
       <Container className="">
@@ -50,11 +65,23 @@ function NewProduct() {
           <Col className=" center">
             <Card className="form-card">
               <Form className="p-5" onSubmit={handleSubmit}>
+                {success ? (
+                  <Alert
+                    key={"upload success"}
+                    variant={"success"}
+                    onClose={() => setSuccess(false)}
+                    dismissible
+                  >
+                    Upload was successful
+                  </Alert>
+                ) : (
+                  <></>
+                )}
                 <h4 className="center">Add New Product</h4>
                 <Form.Group className="mb-3">
                   <Form.Label>Name:</Form.Label>
                   <Form.Control
-                  value={name}
+                    value={name}
                     type="text"
                     placeholder="Enter Item name"
                     required
@@ -65,7 +92,7 @@ function NewProduct() {
                   <Form.Label>Price:</Form.Label>
 
                   <Form.Control
-                  value={price}
+                    value={price}
                     type="text"
                     placeholder="Enter product price"
                     required
@@ -84,7 +111,12 @@ function NewProduct() {
                 </Form.Group>
                 <Form.Group className="mb-3">
                   <Form.Label>Select the image of the product</Form.Label>
-                  <Form.Control type="file" accept="image/*" required onChange={handleImage}/>
+                  <Form.Control
+                    type="file"
+                    accept="image/*"
+                    required
+                    onChange={handleImage}
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3 center">
                   <Button type="submit">Add Product</Button>
