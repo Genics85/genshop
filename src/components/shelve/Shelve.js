87ab5c1245847a140ecc1c32db716c4f7bac7a51
unknown <eugeneamo85@gmail.com>
2023-01-24 15:51:26 +0000

    fetching from api working now, and singleproduct component enhanced

--- src/components/shelve/Shelve.js
+++ src/components/shelve/Shelve.js
@@ -1,25 +1,34 @@
 import { useEffect, useState } from "react";
+import {Col,Container,Row} from "react-bootstrap";
+import "./shelve.scss";
 import axios from "../../api/axios";
 import SingleProduct from "../singleProduct/SingleProduct";
 
 function Shelve() {
- const [items,setItems]=useState([]);
+  const [items, setItems] = useState([]);
 
   const getProducts = async () => {
-    const response = await axios.get("/product/grocery");
-    setItems(response.data);
-    console.log(items);
+    await axios.get("/product/grocery").then((response) => {
+      const listItems = response.data;
+      setItems(listItems);
+      console.log(items);
+    });
   };
 
   useEffect(() => {
     getProducts();
+    console.log(items);
   }, []);
   return (
-    <div>
-      {
-        
-      }
-    </div>
+    <Container className="shelve p-3">
+      {items.map((item) => {
+        return (
+          <Col className="col-12 col-md-3 mb-4">
+            <SingleProduct price={item.price} name={item.name} img={item.img} />
+          </Col>
+        );
+      })}
+    </Container>
   );
 }
 
--- src/components/singleProduct/SingleProduct.js
+++ src/components/singleProduct/SingleProduct.js
@@ -1,18 +1,17 @@
 import "./singleProduct.scss";
-import { Card, Container, Row, Col, Image, Button } from "react-bootstrap";
+import { Card, Col, Image, Button } from "react-bootstrap";
 
 
 function SingleProduct({name,price,img}) {
   return (
     <div>
-              <Container>
-        <Row>
-          <Col className="col-3">
+          <Col className="col-11">
             <Card className="center py-4">
               <Image
                 className="mb-3"
+                style={{height:"100px"}}
                 width={"40%"}
-                src="https://gh.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/06/004223/1.jpg?0775"
+                src={img}
               />
               <p align="center">
                 {name}
@@ -22,12 +21,10 @@ function SingleProduct({name,price,img}) {
               </p>
               <div className="d-flex gap-3">
                 <Button className="buy-button">Buy</Button>
-                <Button>Add to Cart</Button>
+                <Button className="add-to-cart">Add to Cart</Button>
               </div>
             </Card>
           </Col>
-        </Row>
-      </Container>
     </div>
   )
 }
